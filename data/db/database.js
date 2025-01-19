ad071d939630899c62aa6816a535d07b6385736e
Baejiu <jiumaker@naver.com>
2025-01-19 15:20:24 +0900

    fix: fix sequelize db dialect

--- data/db/database.js
+++ data/db/database.js
@@ -4,6 +4,6 @@ import SQ from 'sequelize';
 const { host, user, database, password } = config.db;
 export const sequelize = new SQ.Sequelize(database, user, password, {
   host,
-  dialect: 'mysql',
+  dialect: 'mariadb',
   logging: false,
 });
